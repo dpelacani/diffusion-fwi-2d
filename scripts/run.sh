ff315217c920481fa27c466d64a0c78624b28f61
Dr Deborah Pelacani Cruz <deborah.pelacani@gmail.com>
2026-02-25 16:15:32 +0000

    fix: update resource run with nvidia sdk

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,8 +16,8 @@
 
 #SBATCH --nodes=1                          # Single node (multi-node requires different setup)
 #SBATCH --ntasks=1                         # Single task (accelerate handles multi-GPU internally)
-#SBATCH --ntasks-per-socket=1              # Max tasks per CPU socket
-#SBATCH --cpus-per-task=12                 # CPU cores per task (more for data loading)
+#SBATCH --ntasks-per-socket=2              # Max tasks per CPU socket
+#SBATCH --cpus-per-task=16                 # CPU cores per task (more for data loading)
 #SBATCH --mem=40G                          # Total memory for the job 
 
 
@@ -26,6 +26,9 @@
 export HOME="/scratch_hive/dp4018"
 
 module avail
+module use /opt/nvidia/hpc_sdk/modulefiles
+module load nvhpc
+
 nvidia-smi
 nvcc --version
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -33,7 +36,11 @@ conda activate stride
 
 # These are individual tasks
 # srun python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/training.py
-srun python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/forward.py  
+
+num_nodes=1
+num_workers_per_node=2
+num_threads_per_worker=16
+mrun --local -n $num_nodes -nw $num_workers_per_node -nth $num_threads_per_worker python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/forward.py  &> stride-output.log
 # srun python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/inverse.py
 wait
 
