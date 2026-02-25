f4c77c25424d63a9c52bc499a3e3b19c51b1187a
Dr Deborah Pelacani Cruz <deborah.pelacani@gmail.com>
2026-02-25 19:06:39 +0000

    fix: update SLURM job configuration and enhance script structure for clarity

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,11 +6,12 @@
 
 #SBATCH --job-name=diff-fwi
 #SBATCH --partition=hive
-#SBATCH --qos=hive_flash
+#SBATCH --qos=hive_intermediate
 
 #SBATCH --gres=gpu:1                      # Number of GPUs to allocate to this job
-#SBATCH --time=00:10:00                   # The walltime
+#SBATCH --time=12:00:00                   # The walltime
 
+# Change these paths if you want to redirect output to different files or locations
 #SBATCH -e /scratch_hive/dp4018/scripts/diffusion-fwi-2d/slurm/slurm.err              # File to redirect stderr %j
 #SBATCH -o /scratch_hive/dp4018/scripts/diffusion-fwi-2d/slurm/slurm.out              # File to redirect stdout %j
 
@@ -21,9 +22,9 @@
 #SBATCH --mem=40G                          # Total memory for the job 
 
 
-# This is where the actual work is done.
-
-export HOME="/scratch_hive/dp4018"
+########################################################
+############# Module and Environment Setup #############
+########################################################
 
 module avail
 module use /opt/nvidia/hpc_sdk/modulefiles
@@ -31,17 +32,37 @@ module load nvhpc
 
 nvidia-smi
 nvcc --version
-source ~/miniconda3/etc/profile.d/conda.sh
+source /scratch_hive/dp4018/miniconda3/etc/profile.d/conda.sh # change this path if your conda is installed elsewhere
 conda activate stride
-
-# These are individual tasks
-# srun python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/training.py
 
 num_nodes=1
 num_workers_per_node=2
 num_threads_per_worker=16
-mrun --local -n $num_nodes -nw $num_workers_per_node -nth $num_threads_per_worker python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/forward.py  &> stride-output.log
-# srun python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/inverse.py
+
+
+########################################################
+############# Training the Diffusion model #############
+########################################################
+
+# This performs training of the diffusion model.
+# srun python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/training.py
+
+########################################################
+############## Running Forward Modelling ###############
+########################################################
+
+# # This performs the forward modelling to generate the synthetic data.
+# mrun --local -n $num_nodes -nw $num_workers_per_node -nth $num_threads_per_worker python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/forward.py  &> stride-output.log
+
+########################################################
+############## Running Inverse Modelling ###############
+########################################################
+
+# This performs the inversion to reconstruct the underlying model from the synthetic data, with or without diffusion
+mrun --local -n $num_nodes -nw $num_workers_per_node -nth $num_threads_per_worker python /scratch_hive/dp4018/scripts/diffusion-fwi-2d/scripts/inverse.py  &> stride-output.log
+
+
+
 wait
 
 echo "Job completed successfully."
